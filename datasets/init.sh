5a1da96adef3c805edca57ad931cd71af71e6aaf
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-06-13 21:48:55 +0000

    Added build script

--- datasets/init.sh
+++ datasets/init.sh
@@ -6,4 +6,5 @@ wine_url="http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/
 curl -X GET $wine_url > ./winequality-white.csv;
 
 # Download MNIST Fashion dataset
-training_images="htt
+training_images="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz"
+training_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte
