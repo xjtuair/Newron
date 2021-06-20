6508659c8f25d66175e7c9299e127577d4008ddc
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-06-20 08:41:51 +0000

    fix CSS

--- datasets/init.sh
+++ datasets/init.sh
@@ -7,4 +7,6 @@ curl -X GET $wine_url > ./winequality-white.csv;
 
 # Download MNIST Fashion dataset
 training_images="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz"
-training_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte
+training_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz"
+test_images="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz"
+test_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t
