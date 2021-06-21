26df8f2f2dd69237c75adcd3155f6f1c30260c76
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-06-21 09:39:07 +0000

    dev-java/jetty-jndi: Cleaning old version(s)

--- datasets/init.sh
+++ datasets/init.sh
@@ -9,4 +9,8 @@ curl -X GET $wine_url > ./winequality-white.csv;
 training_images="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz"
 training_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz"
 test_images="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz"
-test_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t
+test_labels="http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz"
+mkdir fashion_mnist;
+
+curl -X GET $training_images > ./fashion_mnist/train-images-idx3-ubyte.gz;
+gzip -d fashion_mnist/train-images-idx3-ubyte.gz;
