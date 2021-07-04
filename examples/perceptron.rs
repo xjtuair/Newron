641f44a8e0e280cb4402753c0a3b5df36dec0780
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-07-04 14:00:29 +0000

    ARM: cns3xxx: fix mapping of private memory region

--- examples/perceptron.rs
+++ examples/perceptron.rs
@@ -8,3 +8,7 @@ use newron::optimizers::sgd::SGD;
 fn main() {
     // Let's create a toy dataset
     let dataset = Dataset::from_raw_data(vec![
+        //   X_0, X_1, X_2, Y
+        vec![1.0, 0.0, 1.0, 1.0],
+        vec![0.0, 1.0, 1.0, 1.0],
+        vec![0.0, 0.0,
