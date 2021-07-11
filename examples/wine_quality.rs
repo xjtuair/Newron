4e73f58ed7b6b42378ae0d08baea5e706f12c92c
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-07-11 14:12:24 +0000

    modified gitignore

--- examples/wine_quality.rs
+++ examples/wine_quality.rs
@@ -3,4 +3,9 @@ use std::path::Path;
 use newron::dataset::Dataset;
 use newron::layers::LayerEnum::*;
 use newron::optimizers::sgd::SGD;
-use newron:
+use newron::sequential::Sequential;
+use newron::loss::{mse::MSE};
+use newron::metrics::Metric;
+
+fn main() {
+    let mut dataset = Dataset::from_csv(Pa
