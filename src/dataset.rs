7defb05f7c43116b2b24c01ffbf1f441e9394abc
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-09-18 21:24:26 +0000

    Prepare for next developments [ci skip]

--- src/dataset.rs
+++ src/dataset.rs
@@ -146,4 +146,8 @@ impl Dataset {
             // read image pixel
             let mut buf = vec![0u8;vector_size];
             images_file.read(&mut buf).unwrap();
-            let mut pixels = utils::to_vec_f6
+            let mut pixels = utils::to_vec_f64(&buf);
+            pixels = pixels.into_iter().map(|x| x / 255.0).collect();
+            // read label
+            let mut label = vec![0u8;1];
+            labels_fil
