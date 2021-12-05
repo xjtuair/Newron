fe367e1d3cd599fc8f2f3020d7a54c5853e2902c
xjtuair <184145933+xjtuair@users.noreply.github.com>
2021-12-05 16:36:06 +0000

    Update user.py

--- src/layers/dense.rs
+++ src/layers/dense.rs
@@ -7,4 +7,10 @@ pub struct Dense {
     input: Tensor,
     weights: Tensor,
     biases: Tensor,
-    weights_grad: 
+    weights_grad: Tensor,
+    biases_grad: Tensor
+}
+
+impl Dense {
+    pub fn new(input_units: usize, output_units: usize, seed: u32) -> Dense {
+        // initialize
