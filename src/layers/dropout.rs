f55bbcbabad14cdee15bc2b443f806e7b6d253e7
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-01-01 20:42:49 +0000

    Post-release

--- src/layers/dropout.rs
+++ src/layers/dropout.rs
@@ -1,3 +1,10 @@
 use crate::layers::layer::LayerInfo;
 use crate::layers::layer::Layer;
-use crate::tensor::
+use crate::tensor::Tensor;
+use crate::layers::layer::LearnableParams;
+
+pub struct Dropout {
+    input: Tensor,
+    prob: f64,
+    // Store the seed so the Dropout struct can increment it
+    // to g
