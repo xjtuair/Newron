479e612f9991ca44e680cb5a15cdad1adcbbe275
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-03-21 19:03:06 +0000

    Rebundle

--- src/layers/tanh.rs
+++ src/layers/tanh.rs
@@ -9,3 +9,51 @@ pub struct TanH {
 }
 
 impl Layer for TanH {
+    fn get_info(&self) -> LayerInfo {
+        LayerInfo {
+            layer_type: format!("tanH"),
+            output_shape: vec![],
+            trainable_param: 0,
+            non_trainable_param: 0,
+        }
+    }
+
+    fn forward(&mut self, input: Tensor, _training: bool) -> Tensor {
+        self.input = input;
+        self.input.map(|x| TanH::tanh(x))
+    }
+
+    fn backward(&mut self, gradient: &Tensor) -> Tensor {
+        let tanh_grad = self.input.map(|x| TanH::tanh_prime(x));
+        gradient.mult_el(&tanh_grad)
+    }
+
+    fn get_params_list(&self) -> Vec<LearnableParams> {
+        vec![]
+    }
+    
+    fn get_grad(&self, _param: &LearnableParams) -> &Tensor {
+        panic!("Layer does not have learnable parameters.")
+    }
+
+    fn get_param(&mut self, _param: &LearnableParams) -> &mut Tensor {
+        panic!("Layer does not have learnable parameters.")
+
+    }
+}
+
+impl TanH {
+    fn tanh(x: f64) -> f64 {
+        x.tanh()
+    }
+
+    fn tanh_prime(x: f64) -> f64 {
+        1.0 - Self::tanh(x).powi(2)
+    }
+
+    pub fn new() -> TanH {
+        TanH {
+            input: Tensor::new(vec![], vec![])
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod dataset;
+pub mod layers;
+pub mod loss;
+pub mod optimizers;
+pub mod metrics;
+pub mod sequential;
+pub
