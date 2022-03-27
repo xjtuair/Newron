dc6bfbe8ff552eadecdb1619983ceaea4b51a7e9
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-03-27 10:03:25 +0000

    Changed documentation

--- src/loss/categorical_entropy.rs
+++ src/loss/categorical_entropy.rs
@@ -1,4 +1,6 @@
 use crate::{tensor::Tensor, loss::loss::Loss, utils, layers::softmax::Softmax};
 pub struct CategoricalEntropy {}
 
-impl 
+impl Loss for CategoricalEntropy {
+    fn compute_loss(&self, y_true: &Tensor, y_pred: &Tensor) -> f64 {
+        let m = y_true.shape[0]
