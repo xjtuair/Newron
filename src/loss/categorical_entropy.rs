10427508366cf53f731950c59c8576785a6f47db
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-03-27 10:33:37 +0000

    This project has been moved to GitLab

--- src/loss/categorical_entropy.rs
+++ src/loss/categorical_entropy.rs
@@ -3,4 +3,8 @@ pub struct CategoricalEntropy {}
 
 impl Loss for CategoricalEntropy {
     fn compute_loss(&self, y_true: &Tensor, y_pred: &Tensor) -> f64 {
-        let m = y_true.shape[0]
+        let m = y_true.shape[0];
+
+        let indices = utils::one_hot_encoded_tensor_to_indices(y_true);
+
+        // Even if last layer is Softmax, we softmax-it again for numerical stab
