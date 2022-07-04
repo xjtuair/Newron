f591bcf07bbfd8f828a9b52e907ee585f5f059ba
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-07-04 12:44:03 +0000

    #1172, fix test case

--- src/utils.rs
+++ src/utils.rs
@@ -3,4 +3,9 @@ use crate::tensor::Tensor;
 
 // Invert integer
 pub(crate) fn swap_endian(val: u32) -> u32 {
-    let result = ((val << 8) & 
+    let result = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
+    return (result << 16) | (result >> 16);
+}
+
+// Convert an array of 4 u8 into a u32
+pub(crate) fn as_u32_le(ar
