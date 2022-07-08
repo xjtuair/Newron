8730de3fc93179f09a21b047db79a4973bf879ec
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-07-08 05:47:44 +0000

    Pull

--- src/utils.rs
+++ src/utils.rs
@@ -8,4 +8,10 @@ pub(crate) fn swap_endian(val: u32) -> u32 {
 }
 
 // Convert an array of 4 u8 into a u32
-pub(crate) fn as_u32_le(ar
+pub(crate) fn as_u32_le(array: &[u8; 4]) -> u32 {
+    ((array[0] as u32) <<  0) +
+    ((array[1] as u32) <<  8) +
+    ((array[2] as u32) << 16) +
+    ((array[3] as u32) << 24)
+}
+
