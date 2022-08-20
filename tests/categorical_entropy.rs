d0387b1af298aafea1e4a4d07041d70ddfddfdd9
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-08-20 20:33:01 +0000

    bluh

--- tests/categorical_entropy.rs
+++ tests/categorical_entropy.rs
@@ -2,4 +2,11 @@
 mod categorical_entropy_tests {
     use newron::tensor::Tensor;
     use newron::loss::loss::Loss;
-    use newron::loss::categorical_entropy::Categorical
+    use newron::loss::categorical_entropy::CategoricalEntropy;
+    use newron::utils;
+
+    #[test]
+    fn test_categorical_entropy_loss() {
+        let loss = CategoricalEntropy{};
+
+        // Test 3 d
