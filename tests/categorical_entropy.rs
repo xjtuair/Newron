8f9003d0bace3d5e2a824cab2d719f3d16fdd20b
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-08-25 08:15:31 +0000

    Fix: README.md

--- tests/categorical_entropy.rs
+++ tests/categorical_entropy.rs
@@ -12,4 +12,6 @@ mod categorical_entropy_tests {
         // Test 3 dimensions (batch = 3 samples)
         let predictions = Tensor::new(vec![0.2, 0.6, 0.2,
                                                          0.8, 0.2, 0.0,
-      
+                                                         1.0, 0.0, 0.0,
+                                                         0.5, 0.5, 0.0,
+  
