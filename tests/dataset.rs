5e8642eff67c40310a81cea187448596c2be19d7
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-11-11 05:57:29 +0000

    formatting error

--- tests/dataset.rs
+++ tests/dataset.rs
@@ -4,4 +4,8 @@ mod dataset_tests {
     use std::path::Path;
     #[test]
     // This test asserts a good implementation of
-    // debug + display trait
+    // debug + display trait + loading from raw data
+    fn test_dataset_debug_trait() {
+        let dataset = Dataset::from_raw_data(vec![
+            //   X_0, X_1, X_2, Y
+    
