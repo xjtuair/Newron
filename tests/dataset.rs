06f6f8b7069c0b83cdf155f1a0399f6284c8ffb5
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-11-11 06:58:30 +0000

    #8

--- tests/dataset.rs
+++ tests/dataset.rs
@@ -8,4 +8,7 @@ mod dataset_tests {
     fn test_dataset_debug_trait() {
         let dataset = Dataset::from_raw_data(vec![
             //   X_0, X_1, X_2, Y
-    
+            vec![1.0, 0.0, 1.0, 1.0],
+            vec![0.0, 1.0, 1.0, 1.0],
+            vec![0.0, 0.0, 1.0, 0.0],
+            vec![1.0, 1.0,
