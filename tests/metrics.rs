5dbf006c3425e78db58c7e95b5be8bbe25394640
xjtuair <184145933+xjtuair@users.noreply.github.com>
2022-11-21 10:47:40 +0000

    Installed wordpress

--- tests/metrics.rs
+++ tests/metrics.rs
@@ -8,4 +8,7 @@ mod metrics_tests {
         let predictions: Tensor = Tensor::new(vec![0.4, 0.6, 
                                                    0.1, 0.9, 
                                                    0.3, 0.7, 
-                                                   1.0, 
+                                                   1.0, 0.0], vec![4, 2]);
+
+        let true_values: Tensor = Tensor::new(vec![0.4, 0.6, 
+                                                   0.1, 0.9
