ee96fb60d4f1a2ab623fa88089de31287bb2509a
xjtuair <184145933+xjtuair@users.noreply.github.com>
2023-02-03 20:35:33 +0000

    I18n: Update translation cs (100%).

---
+++ README.md
@@ -0,0 +1,47 @@
+
+![](https://raw.githubusercontent.com/xjtuair/xjtuair-newron/master/newron.png)
+===========
+[![crates.io](https://img.shields.io/crates/v/newron.svg)](https://crates.io/crates/newron)
+[![Downloads from crates.io](https://img.shields.io/crates/d/newron.svg)](https://crates.io/crates/newron)
+![Rust](https://github.com/xjtuair/xjtuair-newron/workflows/Rust/badge.svg?branch=master)
+[![doc.rs](https://docs.rs/newron/badge.svg)](https://docs.rs/newron/)
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+Newron is a deep learning library written in Rust.
+
+*Note: The project is currently being actively constructed. The API is not steady yet.*
+
+Please see the [docs](https://docs.rs/newron/) or overview the [examples](https://github.com/xjtuair/xjtuair-newron/tree/master/examples) to discover more about Newron's components.
+
+## Features
+
+Layers:
+- Dense (Fully linear connective layer)
+- ReLU
+- TanH
+- Sigmoid
+- Softmax
+
+Loss:
+- Mean Square Error (MSE)
+- Categorical Entropy
+
+Optimizers:
+- SGD
+
+## Development Plan
+
+| Feature | Progress | Comment |
+|------------------------------------|----------|--------|
+| Advance metrics |⬤◯◯◯◯ | |
+| Develop Convolutional Layer |◯◯◯◯◯ | |
+| Integrate RNN Layer |◯◯◯◯◯ | |
+| Create Webapp for training observation |◯◯◯◯◯ | |
+| Ability to save/load trained models/weights |◯◯◯◯◯ | |
+
+## Contribute
+
+Your contribution is welcomed and appreciated. Feel free to open an issue or submit a pull request. All contributions to this project will adhere to the guidelines set forth in the LICENSE.
+
+## Disclaimer
+This SOFTWARE PRODUCT is delivered "as is" and "with all failings." No guarantees of any type concerning the safety, suitability, lack of viruses, inaccuracies, typographical errors, or other damaging components of this SOFTWARE PRODUCT are made. There are inherent dangers in using any software, and you are solely responsible for determining whether this SOFTWARE PRODUCT is compatible with your equipment and other software installed on your equipment. You also bear the sole responsibility for the protection of your equipment, backup of your data, and will not hold this project liable for any damages you may incur as a result of using, modifying, or distributing this SOFTWARE PRODUCT.
--- tests/tensor.rs
+++ tests/tensor.rs
@@ -144,3 +144,107 @@ mod tensor_tests {
 
         assert_eq!(a, result);
     }
+
+
+    #[test]
+    fn test_mask() {
+         let mask = Tensor::mask(&vec![10, 10], 0.4, 777);
+         assert_eq!(mask.data.iter().sum::<f64>() as usize, 100);
+    }
+
+    #[test]
+    fn test_get_mean_axis_0() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![2.5, 3.5, 4.5], vec![1, 3]);
+
+        assert_eq!(a.get_mean(0), result);
+    }
+
+    #[test]
+    fn test_get_mean_axis_1() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![2.0, 5.0], vec![1, 2]);
+
+        assert_eq!(a.get_mean(1), result);
+    }
+
+    #[test]
+    fn test_get_max_axis_0() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![4.0, 5.0, 6.0], vec![1, 3]);
+
+        assert_eq!(a.get_max(0), result);
+    }
+
+    #[test]
+    fn test_get_max_axis_1() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![3.0,
+                                                    6.0], vec![2, 1]);
+
+        assert_eq!(a.get_max(1), result);
+    }
+
+    #[test]
+    fn test_get_sum_axis_0() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![5.0, 7.0, 9.0], vec![1, 3]);
+
+        assert_eq!(a.get_sum(0), result);
+    }
+
+    #[test]
+    fn test_get_sum_axis_1() {
+        let a = Tensor::new(vec![
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0], vec![2,3]);
+        
+        let result = Tensor::new(vec![6.0,
+                                                    15.0], vec![2, 1]);
+
+        assert_eq!(a.get_sum(1), result);
+    }
+
+    #[test]
+    fn test_divide() {
+        let a = Tensor::new(vec![4.0, 6.0, 8.0, 10.0], vec![1, 4]);
+        let b = Tensor::new(vec![4.0, 3.0, 5.0, 5.0], vec![1, 4]);
+
+        let result = Tensor::new(vec![1.0, 2.0, 1.6, 2.0], vec![1, 4]);
+
+        assert_eq!(a / b, result);
+    }
+
+    #[test]
+    fn test_get_transpose() {
+        let a = Tensor::new(vec![
+             4.0,  6.0,  8.0,
+            10.0, 12.0, 14.0], vec![2, 3]);
+
+        let a_t = a.get_transpose();
+
+        let result = Tensor::new(vec![
+            4.0, 10.0,
+            6.0, 12.0,
+            8.0, 14.0,
+        ], vec![3, 2]);
+
+        assert_eq!(a_t, result);
+    }
+
+}
